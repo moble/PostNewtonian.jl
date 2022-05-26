8fda69f6f0078b4879f1f82a15a2959ee1c1656d
Documenter.jl <documenter@juliadocs.github.io>
2022-05-26 02:43:22 +0000

    build based on 3052d6f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PostNewtonian","category":"page"},{"location":"#PostNewtonian","page":"Home","title":"PostNewtonian","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PostNewtonian.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PostNewtonian]","category":"page"}]
+}
