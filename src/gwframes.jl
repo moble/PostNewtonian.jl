810c94db3b06870557887528437b90b13f22ec11
user.email <michael.oliver.boyle@gmail.com>
2022-07-18 17:57:19 -0400

    Set final time to 0

---
+++ src/gwframes.jl
@@ -0,0 +1,166 @@
+module GWFrames
+
+using ..PostNewtonian
+using Quaternionic
+using DataInterpolations: CubicSpline
+
+
+"""
+    PNWaveform(Approximant, delta, chi1_i, chi2_i, Omega_orb_i; kwargs...)
+
+Compute a PN waveform, with the same call signature as `GWFrames.PNWaveform`
+
+This is essentially a compatibility layer for the corresponding function in
+[`GWFrames`](https://github.com/moble/GWFrames/blob/01b39bfe/Code/PNWaveforms.cpp#L83-L88),
+with one additional optional argument: `dt` (see below).
+
+!!! warning
+
+    We do *not* expect the result of this function to be identical to the
+    result from `GWFrames`.  In particular, this package uses more general
+    expressions for the tidal heating terms, fixed an error in the 2PN
+    quadratic-spin terms for the waveform modes, and uses more accurate (and
+    efficient) ODE integration.
+
+The Julia interface is more detailed, flexible, and efficient than the simple
+`GWFrames` interface that this function emulates.  In particular,
+[`inspiral`](@ref) takes essentially all the same arguments that
+[`DifferentialEquations.solve`](https://diffeq.sciml.ai/dev/basics/common_solver_opts/)
+takes, and returns a [`solution`](https://diffeq.sciml.ai/dev/basics/solution/)
+that provides dense output and more details about the ODE solution itself.  For
+example, one reason this function is more efficient than `GWFrames` is that we
+can use dense output to solve with fewer timesteps, while accurately and
+efficiently interpolating to the requested timesteps.  While `inspiral` solves
+for the dynamics, [`h!`](@ref) provides the actual mode weights, which are also
+returned by this function.
+
+
+## Required arguments
+
+  * `Approximant`: Currently, only `TaylorT1` is supported.
+  * `delta`: Fractional mass difference ``(M₁-M₂)/(M₁+M₂)``
+  * `chi1_i`: Normalized spin vector ``S⃗₁/M₁²``
+  * `chi2_i`: Normalized spin vector ``S⃗₂/M₂²``
+  * `Omega_orb_i`: Orbital angular frequency at initial instant
+
+
+## Keyword arguments
+
+  * `Omega_orb_0=Omega_orb_i`: Orbital angular frequency at first instant found
+    in data.  If this is less than `Omega_orb_i`, the system is integrated
+    backwards in time from the latter value to this value.
+  * `R_frame_i=Rotor(1)`: Initial orientation of the frame.
+  * `MinStepsPerOrbit=32`: (Approximate) minimum number of time steps in the
+    output data per orbit.  Because the waveform modes go as high as ``m=8``,
+    this number should be at least 16 to avoid Nyquist aliasing in those modes.
+    Note that this value may be overridden by `dt` (see below).
+  * `PNWaveformModeOrder=3.5`: Maximum PN order of terms in the
+    waveform formulas.  Currently, only 3.5 is supported.
+  * `PNOrbitalEvolutionOrder=4.0`: Maximum PN order of terms in the
+    orbital-evolution formulas.  Currently, only 4.0 is supported.
+  * `dt=0`: Uniform time step size of the output.  If this is not a strictly
+    positive number, `MinStepsPerOrbit` will be used instead.
+
+
+## Returned values
+
+This function returns a NamedTuple with the following keys:
+
+  * `t`: The vector of time steps at which the data are evaluated
+  * `data`: Matrix of complex values of the mode weights.  Note that this
+    matrix is essentially transposed in python.  The first dimension enumerates
+    the modes, starting with ``(2,-2)``, then ``(2,-1)``, up to ``(2,2)`,
+    followed by ``(3,-3)``, and so on up to ``(8,8)``.  The second dimension
+    enumerates the values at each instant of time.  This is the same ordering
+    as results from `GWFrames`, but opposite to the ordering used by the `sxs`
+    and `scri` packages.  However, also note that conversion between Julia and
+    Python will frequently automatically transpose matrices, because Julia is
+    Fortran-ordered by default, whereas numpy is C-ordered.  It is best to
+    check the shape manually to be sure.
+  * `frame`: Matrix of shape 4 x length(t) representing the frame-orientation
+    quaternion as a function of time `t`.
+  * `M1`, `M2`: Vectors of the respective masses as functions of time `t`.
+    Note that only at the time corresponding to `Omega_orb_i` will the total
+    mass be precisely 1.  Generally, tidal heating will lead to time-dependent
+    masses.
+  * `chi1`, `chi2`: Matrices of shape 3 x length(t) representing the spins as
+    functions of time `t`.
+  * `v`: PN velocity parameter as a function of time `t`.
+  * `Phi`: Orbital phase as a function of time `t`.
+
+"""
+function PNWaveform(
+    Approximant::String, delta::Float64, chi1_i::Vector{Float64}, chi2_i::Vector{Float64}, Omega_orb_i::Float64,
+    Omega_orb_0::Float64=Omega_orb_i, R_frame_i::Vector{Float64}=[1.0], MinStepsPerOrbit::Integer=32,
+    PNWaveformModeOrder::Float64=3.5, PNOrbitalEvolutionOrder::Float64=4.0, dt::Float64=0.0
+)
+    PNWaveform(
+        Approximant, delta, chi1_i, chi2_i, Omega_orb_i;
+        Omega_orb_0, R_frame_i, MinStepsPerOrbit,
+        PNWaveformModeOrder, PNOrbitalEvolutionOrder, dt
+    )
+end
+function PNWaveform(
+    Approximant::String, delta::Float64, chi1_i::Vector{Float64}, chi2_i::Vector{Float64}, Omega_orb_i::Float64;
+    Omega_orb_0::Float64=Omega_orb_i, R_frame_i::Vector{Float64}=[1.0], MinStepsPerOrbit::Integer=32,
+    PNWaveformModeOrder::Float64=3.5, PNOrbitalEvolutionOrder::Float64=4.0, dt::Float64=0.0
+)
+    if Approximant != "TaylorT1"
+        @error "`Approximant` other than \"TaylorT1\" is not yet supported"
+    end
+    if PNWaveformModeOrder ≉ 3.5
+        @error "`PNWaveformModeOrder` other than 3.5 is not yet supported"
+    end
+    if PNOrbitalEvolutionOrder ≉ 4.0
+        @error "`PNOrbitalEvolutionOrder` other than 4.0 is not yet supported"
+    end
+    M₁ = (1 + delta) / 2
+    M₂ = (1 - delta) / 2
+    χ⃗₁ = QuatVec(chi1_i...)
+    χ⃗₂ = QuatVec(chi2_i...)
+    Ωᵢ = Omega_orb_i
+    Ω₁ = Omega_orb_0
+    Rᵢ = Rotor(R_frame_i...)
+
+    # Inspiral
+    solution = inspiral(
+        M₁, M₂, χ⃗₁, χ⃗₂, Ωᵢ;
+        Ω₁=Ω₁, Rᵢ=Rᵢ,
+        PNSys=TaylorT1, PNOrder=7//2,
+        integrate_orbital_phase=true,
+        saveat=dt > 0 ? dt : []
+    )
+    if dt ≤ 0
+        solution = let
+            Φ = solution[end, :]
+            t = solution.t
+            δΦ = 2π / MinStepsPerOrbit
+            Φrange = range(extrema(Φ)..., step=δΦ)
+            t_Φ = CubicSpline(t, Φ).(Φrange)
+            solution(t_Φ)
+        end
+    end
+
+    # Modes
+    n_modes = 77
+    h = Matrix{Complex{eltype(solution)}}(undef, n_modes, length(solution.t))
+    for (hi, ui) in zip(axes(h, 2), solution.u)
+        h!(@view(h[:, hi]), ui[1:13]; ℓmin=2)
+    end
+
+    # Return
+    (
+        t=solution.t .- solution.t[end],
+        data=h,
+        frame=solution[9:12, :], # R
+        M1=solution[1, :], # M₁
+        M2=solution[2, :], # M₂
+        chi1=solution[3:5, :], # χ⃗₁
+        chi2=solution[6:8, :], # χ⃗₂
+        v=solution[13, :], # v
+        Phi=solution[14, :], # Φ
+    )
+end
+
+
+end  # module GWFrames
