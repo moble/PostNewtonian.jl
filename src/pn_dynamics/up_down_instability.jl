fb0c641a6a9bd28ed68056a98c6005a977faecc2
user.email <michael.oliver.boyle@gmail.com>
2023-03-12 15:57:55 -0400

    Mostly work on docstrings

--- src/pn_dynamics/up_down_instability.jl
+++ src/pn_dynamics/up_down_instability.jl
@@ -1,30 +1,27 @@
 """
     up_down_instability(pnstate)
 
-Compute the range of frequencies over which the system is unstable to
-increasing precession.
+Compute the range of frequencies over which the system is unstable to increasing precession.
 
-The returned value is a pair of dimensionless frequencies giving the lower and
-upper frequencies between which we can expect instability.  If there is no
-instability expected, the returned pair is just (1,1).
+The returned value is a pair of dimensionless frequencies giving the lower and upper
+frequencies between which we can expect instability.  If there is no instability expected,
+the returned pair is just (1,1).
 
-For compact binaries in which the spins are either aligned or anti-aligned with
-the orbital angular velocity, we do not expect any precession effects — simply
-by symmetry.  However, if the spin of the higher-mass object is aligned with
-the orbital angular velocity and the spin of the lower-mass object is
-anti-aligned, the binary is unstable to precession — meaning that any minuscule
-misalignment can grow rapidly into significant precession.  This was first
-reported by [Gerosa et al. (2015)](http://arxiv.org/abs/1506.09116), and the
-range over which the system is unstable is given by Eq. (2) of that reference.
-We use the lowest-order approximation to convert binary separation to
-frequency.  The result is also "clamped" between 0 and 1, because sometimes the
-PN approximations involved break down and return unphysical values.
-
+For compact binaries in which the spins are either aligned or anti-aligned with the orbital
+angular velocity, we do not expect any precession effects — simply by symmetry.  However, if
+the spin of the higher-mass object is aligned with the orbital angular velocity and the spin
+of the lower-mass object is anti-aligned, the binary is unstable to precession — meaning
+that any minuscule misalignment can grow rapidly into significant precession.  This was
+first reported by [Gerosa et al. (2015)](http://arxiv.org/abs/1506.09116), and the range
+over which the system is unstable is given by Eq. (2) of that reference.  We use the
+lowest-order approximation to convert binary separation to frequency.  The result is also
+"clamped" between 0 and 1, because sometimes the PN approximations involved break down and
+return unphysical values.
 """
 @compute_pn_variables function up_down_instability(pnstate)
     T = typeof(χ₁ₗ)
     if M₂ ≤ M₁
-        q = 1 / q
+        q = inv(q)
         χ₂ₗ, χ₁ₗ = χ₁ₗ, χ₂ₗ
     end
     if χ₁ₗ > 0 && χ₂ₗ < 0
--- src/pn_variables/fundamental_variables.jl
+++ src/pn_variables/fundamental_variables.jl
@@ -2,13 +2,17 @@ module FundamentalVariables
 
 using ..PostNewtonian: PNState
 
-export M₁, M₂, χ⃗₁, χ⃗₂, R, v
+export M₁, M₂, χ⃗₁, χ⃗₂, R, v, Φ
 
-M₁(s::PNState) = s.u[1]
-M₂(s::PNState) = s.u[2]
-χ⃗₁(s::PNState) = QuatVec(s.u[3:5]...)
-χ⃗₂(s::PNState) = QuatVec(s.u[6:8]...)
-R(s::PNState) = Rotor(s.u[9:12]...)
-v(s::PNState) = s.u[13]
+M₁(s::PNState) = @inbounds s.u[1]
+M₂(s::PNState) = @inbounds s.u[2]
+χ⃗₁(s::PNState) = @inbounds QuatVec(s.u[3:5]...)
+χ⃗₂(s::PNState) = @inbounds QuatVec(s.u[6:8]...)
+R(s::PNState) = @inbounds Rotor(s.u[9:12]...)
+v(s::PNState) = @inbounds s.u[13]
+Φ(s::PNState) = s.u[14]  # NO @inbounds
+
+λ₁(::PNState) = 0
+λ₂(::PNState) = 0
 
 end
