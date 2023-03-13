f929a286be6f9d00fdfccb4651fe44b608497d2a
user.email <michael.oliver.boyle@gmail.com>
2023-03-13 10:20:16 -0400

    Use `state` instead of `u`

--- src/pn_variables/fundamental_variables.jl
+++ src/pn_variables/fundamental_variables.jl
@@ -4,13 +4,13 @@ using ..PostNewtonian: AbstractPNSystem
 
 export M₁, M₂, χ⃗₁, χ⃗₂, R, v, Φ
 
-M₁(s::AbstractPNSystem) = @inbounds s.u[1]
-M₂(s::AbstractPNSystem) = @inbounds s.u[2]
-χ⃗₁(s::AbstractPNSystem) = @inbounds QuatVec(s.u[3:5]...)
-χ⃗₂(s::AbstractPNSystem) = @inbounds QuatVec(s.u[6:8]...)
-R(s::AbstractPNSystem) = @inbounds Rotor(s.u[9:12]...)
-v(s::AbstractPNSystem) = @inbounds s.u[13]
-Φ(s::AbstractPNSystem) = s.u[14]  # NO @inbounds
+M₁(s::AbstractPNSystem) = @inbounds s.state[1]
+M₂(s::AbstractPNSystem) = @inbounds s.state[2]
+χ⃗₁(s::AbstractPNSystem) = @inbounds QuatVec(s.state[3:5]...)
+χ⃗₂(s::AbstractPNSystem) = @inbounds QuatVec(s.state[6:8]...)
+R(s::AbstractPNSystem) = @inbounds Rotor(s.state[9:12]...)
+v(s::AbstractPNSystem) = @inbounds s.state[13]
+Φ(s::AbstractPNSystem) = s.state[14]  # NO @inbounds
 
 λ₁(::AbstractPNSystem) = 0
 λ₂(::AbstractPNSystem) = 0
