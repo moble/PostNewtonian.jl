86d03034ad9a3a11a93ff05faa50564ccbe7c7ac
user.email <michael.oliver.boyle@gmail.com>
2023-03-18 16:13:40 -0400

    Separate macros from functions

--- src/utilities/macros.jl
+++ src/utilities/macros.jl
@@ -29,24 +29,18 @@ function type_converter(pnsystem, x)
     convert(eltype(pnsystem), x)
 end
 
-function compute_pn_variables(arg_index, func)
-    splitfunc = MacroTools.splitdef(func)
-    pnsystem = esc(MacroTools.namify(splitfunc[:args][arg_index]))
-    body = splitfunc[:body]
-
-    x = esc(:x)  # Used as a dummy variable in anonymous functions below
-
+function compute_pn_variables(pnsystem::Symbol, body)
     irrationals_exprs = [
         :($v=type_converter($pnsystem, $v))
-        for v ∈ esc.(filter(v->MacroTools.inexpr(body, v), irrationals))
+        for v ∈ filter(v->MacroTools.inexpr(body, v), irrationals)
     ]
     pnvariables_exprs = [
         :($v=$v($pnsystem))
-        for v ∈ esc.(filter(v->MacroTools.inexpr(body, v), pnvariables))
+        for v ∈ filter(v->MacroTools.inexpr(body, v), pnvariables)
     ]
     unary_funcs_exprs = [
-        :($v=($x->$v(type_converter($pnsystem, $x))))
-        for v ∈ esc.(filter(v->MacroTools.inexpr(body, v), unary_funcs))
+        :($v=(x->$v(type_converter($pnsystem, x))))
+        for v ∈ filter(v->MacroTools.inexpr(body, v), unary_funcs)
     ]
     exprs = [
         irrationals_exprs;
@@ -55,13 +49,18 @@ function compute_pn_variables(arg_index, func)
     ]
 
     new_body = quote
+        #@fastmath let $(exprs...)
         let $(exprs...)
-            $(esc(body))
+            $(body)
         end
     end
+end
 
-    splitfunc[:args][arg_index] = pnsystem
-    splitfunc[:body] = new_body
+function compute_pn_variables(arg_index::Integer, func)
+    splitfunc = MacroTools.splitdef(func)
+    pnsystem = MacroTools.namify(splitfunc[:args][arg_index])
+    body = splitfunc[:body]
+    splitfunc[:body] = compute_pn_variables(pnsystem, body)
     MacroTools.combinedef(splitfunc)
 end
 
@@ -74,15 +73,16 @@ present defines them appropriately inside that function.
 The first argument to this macro is `arg_index`, which just tells us which argument to the
 function `func` is a `PNSystem`.  For example, the variables defined in
 [`PostNewtonian.FundamentalVariables`](@ref Fundamental variables) all take a single
-argument of `pnsystem`, which is used to compute the values for those variables.  The macro
+argument of `pnsystem`, which is used to compute the values for those variables; this macro
 just needs to know where to find `pnsystem`.
 
 Once it has this information, there are three types of transformations it will make:
 
  1. For every [fundamental](@ref Fundamental variables) or [derived](@ref Derived variables)
-    variable, the name of that variable used in the `func` will be replaced by its value
-    when called with `pnsystem`.  For example, you can simply use [`M₁`](@ref) or
-    [`μ`](@ref) in your code without have to call them as `M₁(pnsystem)` or `μ(pnsystem)`.
+    variable, the name of that variable used in the body of `func` will be replaced by its
+    value when called with `pnsystem`.  For example, you can simply use the symbols `M₁` or
+    `μ` in your code, rather than calling them as [`M₁(pnsystem)`](@ref M₁) or
+    [`μ(pnsystem)`](@ref μ) every time they appear.
  2. Every `Irrational` defined in `Base.MathConstants` or `PostNewtonian.MathConstants` will
     be transformed to the `eltype` of `pnsystem`.  This lets you naturally use such
     constants in expressions like `2π/3` without automatically converting to `Float64`.
@@ -95,9 +95,9 @@ placed around the body of `func`, so that the values may be used efficiently wit
 recomputation.
 """
 macro compute_pn_variables(func)
-    compute_pn_variables(1, func)
+    esc(compute_pn_variables(1, func))
 end
 
 macro compute_pn_variables(arg_index, func)
-    compute_pn_variables(arg_index, func)
+    esc(compute_pn_variables(arg_index, func))
 end
