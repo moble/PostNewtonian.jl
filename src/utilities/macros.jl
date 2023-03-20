86eb07029c2a3066ae7e09e1ddb9ef9987a0b144
user.email <michael.oliver.boyle@gmail.com>
2023-03-20 13:05:52 -0400

    Rearrange code for clearer reading

--- src/utilities/macros.jl
+++ src/utilities/macros.jl
@@ -1,3 +1,50 @@
+"""
+    hold(x)
+
+Delay evaluation of the argument in `Symbolics` expressions.
+
+This is just a helper function that acts trivially — like the `identity` function — but also
+gets registered with `Symbolics` to avoid evaluation of the argument.  For example, we can
+preserve expressions like `π^2`, which Julia would normally convert directly to a `Float64`.
+
+Note that you probably don't want to use this function directly; this will probably be done
+for you by [`@pn_expression`](@ref) or similar.  If you *do* want to use this directly, you
+probably want another layer of indirection to construct something like
+`Symbolics.Num(SymbolicUtils.Term(hold, [x]))` so that you can use the result in a symbolic
+expression.
+"""
+hold(x) = x
+@register_symbolic hold(x)
+Symbolics.derivative(::typeof(hold), args::NTuple{1,Any}, ::Val{1}) = 1
+
+"""
+    type_converter(pnsystem, x)
+
+Convert `x` to a type appropriate for the float type of `pnsystem`.
+"""
+function type_converter(::PNSystem{T}, x) where {T<:Num}
+    Symbolics.Num(SymbolicUtils.Term(hold, [x]))
+end
+function type_converter(::PNSystem{T}, x::Num) where {T<:Num}
+    x
+end
+function type_converter(pnsystem, x)
+    convert(eltype(pnsystem), x)
+end
+
+"""
+    unhold(expr)
+
+Remove occurrences of [`hold`](@ref) from an `Expr`.
+"""
+function unhold(expr)
+    MacroTools.postwalk(expr) do x
+        m = MacroTools.trymatch(:(f_(i_)), x)
+        m === nothing || m[:f]!==hold ? x : Symbol(m[:i])
+    end
+end
+
+
 fundamental_variables = methodswith(PNSystem, FundamentalVariables)
 derived_variables = methodswith(PNSystem, DerivedVariables)
 pnvariables = map(v->v.name, [fundamental_variables; derived_variables])
@@ -19,44 +66,30 @@ irrationals = unique([
 
 unary_funcs = [:√, :sqrt, :log, :ln, :sin, :cos]
 
-hold(x) = x
-@register_symbolic hold(x)
-Symbolics.derivative(::typeof(hold), args::NTuple{1,Any}, ::Val{1}) = 1
-function type_converter(::PNSystem{T}, x) where {T<:Num}
-    Symbolics.Num(SymbolicUtils.Term(hold, [x]))
-end
-function type_converter(::PNSystem{T}, x::Num) where {T<:Num}
-    x
-end
-function type_converter(pnsystem, x)
-    convert(eltype(pnsystem), x)
-end
-function unhold(expr)
-    MacroTools.postwalk(expr) do x
-        m = MacroTools.trymatch(:(f_(i_)), x)
-        m === nothing || m[:f]!==hold ? x : Symbol(m[:i])
-    end
-end
 
 function pn_expression(pnsystem::Symbol, body)
-    irrationals_exprs = [
-        :($v=type_converter($pnsystem, $v))
-        for v ∈ filter(v->MacroTools.inexpr(body, v), irrationals)
-    ]
+    # Look for variables in `body` that we need to treat specially, and write exprs to do
+    # so.  These three are described as bullet points in the docstring of `@pn_expression`.
     pnvariables_exprs = [
         :($v=$v($pnsystem))
         for v ∈ filter(v->MacroTools.inexpr(body, v), pnvariables)
+    ]
+    irrationals_exprs = [
+        :($v=type_converter($pnsystem, $v))
+        for v ∈ filter(v->MacroTools.inexpr(body, v), irrationals)
     ]
     unary_funcs_exprs = [
         :($v=(x->$v(type_converter($pnsystem, x))))
         for v ∈ filter(v->MacroTools.inexpr(body, v), unary_funcs)
     ]
+
     exprs = [
-        irrationals_exprs;
         pnvariables_exprs;
+        irrationals_exprs;
         unary_funcs_exprs
     ]
 
+    # Now, just wrap `body` in a `let` block, where we include the exprs created above
     new_body = quote
         @fastmath let $(exprs...)
             $(body)
