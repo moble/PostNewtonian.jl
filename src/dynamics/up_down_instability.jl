e6d191a3bc42460c0cc02dd6f2d2840c3d993ce8
user.email <michael.oliver.boyle@gmail.com>
2023-03-24 14:46:06 -0400

    Rewrite internals in terms of v; add up_down_instability_warn

--- src/dynamics/up_down_instability.jl
+++ src/dynamics/up_down_instability.jl
@@ -5,7 +5,8 @@ Compute the range of frequencies over which the system is unstable to increasing
 
 The returned value is a pair of dimensionless frequencies giving the lower and upper
 frequencies between which we can expect instability.  If there is no instability expected,
-the returned pair is just (1,1) — where 1 is the limit of physically reasonable frequencies.
+the returned pair is just (1/M, 1/M) — where ``M`` is the total mass of the system, and
+``1/M`` is the upper limit of physically reasonable frequencies.
 
 For compact binaries in which the spins are either aligned or anti-aligned with the orbital
 angular velocity, we do not expect any precession effects — simply by symmetry.  However, if
@@ -15,11 +16,12 @@ that any minuscule misalignment can grow rapidly into significant precession.  T
 first reported by [Gerosa et al. (2015)](http://arxiv.org/abs/1506.09116), and the range
 over which the system is unstable is given by Eq. (2) of that reference.  We use the
 lowest-order approximation to convert binary separation to frequency.  The result is also
-"clamped" between 0 and 1, because sometimes the PN approximations involved break down and
-return unphysical values.
+"clamped" between ``0`` and ``1/M``, because sometimes the PN approximations involved break
+down and return values outside of those physically plausible limits.
 """
 @pn_expression function up_down_instability(pnsystem)
-    T = typeof(χ₁ₗ)
+    T = eltype(pnsystem)
+    Ωₘₐₓ = Ω(v=1, M=M)
     if M₂ ≤ M₁
         q = inv(q)
         χ₂ₗ, χ₁ₗ = χ₁ₗ, χ₂ₗ
@@ -29,8 +31,39 @@ return unphysical values.
         r₋ = M * (√(χ₁ₗ) - √abs(q*χ₂ₗ))^4 / (1-q)^2
         Ω₊ = √(M/r₊)^3
         Ω₋ = √(M/r₋)^3
-        clamp.((Ω₊, Ω₋), T(0), T(1))
+        clamp.((Ω₊, Ω₋), T(0), Ωₘₐₓ)
     else
-        (T(1), T(1))
+        (Ωₘₐₓ, Ωₘₐₓ)
+    end
+end
+
+
+@doc raw"""
+    function up_down_instability_warn(pnsystem, v₁, vₑ, vₗᵢₘᵢₜ=1//2)
+
+If this system is likely to encounter the up-down instability, log a warning with details.
+
+This function issues the warning if the system is reasonably non-precessing (``\chi_\perp
+\leq 10^{-2}``) in its current configuration (as given by `pnsystem`), and the range of
+frequencies `(v₁, vₑ)` over which it will be integrated is likely to encounter the up-down
+instability — except that frequencies above `vₗᵢₘᵢₜ` will be ignored, as PN is likely to
+have broken down anyway.
+
+See [`up_down_instability`](@ref) for details of the calculation of the unstable region.
+"""
+@pn_expression function up_down_instability_warn(pnsystem, v₁, vₑ, vₗᵢₘᵢₜ=1//2)
+    if χₚₑᵣₚ ≤ 1e-2
+        (Ω₊, Ω₋) = up_down_instability(pnsystem)
+        v₊, v₋ = v(Ω=Ω₊, M=M), v(Ω=Ω₋, M=M)
+        if v₁ < min(v₋, vₗᵢₘᵢₜ) && min(vₑ, vₗᵢₘᵢₜ) > v₊
+            @warn (
+                "This system is likely to encounter the up-down instability in the\n"
+                * "frequency range (Ω₊, Ω₋)=$((Ω₊, Ω₋)),\ncorresponding "
+                * "to the range of PN velocity parameters (v₊, v₋)=$((v₊, v₋)).\n"
+                * "This is a true physical instability; not just a numerical issue.\n"
+                * "Despite the initial conditions containing very small precession,\n"
+                * "the system will likely evolve to have very large precession."
+            ) M₁ M₂ χ⃗₁ χ⃗₂ R v v₁ vₑ
+        end
     end
 end
