3b345622ebd2ca2e258ef95ccb4a85e181cb2212
user.email <michael.oliver.boyle@gmail.com>
2023-03-24 08:43:22 -0400

    Include `randn` method

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,7 +1,7 @@
+include("utilities/mathconstants.jl")
 include("utilities/misc.jl")
 include("utilities/combine_solutions.jl")
 include("utilities/termination_criteria.jl")
-include("utilities/mathconstants.jl")
 
 # Don't include macros.jl yet; we need to get contents of `FundamentalVariables` and
 # `DerivedVariables`, so that's included by `pn_expressions.jl`.
