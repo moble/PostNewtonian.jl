04b16c29b72c16c9362f14a2148ea973cdd7758c
user.email <michael.oliver.boyle@gmail.com>
2023-03-27 21:40:38 -0400

    Be more specific about expected values

--- test/up_down_instability.jl
+++ test/up_down_instability.jl
@@ -6,17 +6,23 @@
     χ⃗₁ = QuatVec(-6.25908582173e-09, -2.21655853316e-08,  0.723734029888)
     χ⃗₂ = QuatVec( 3.39881185355e-08,  5.92671870568e-08, -0.799746224993)
     R = Rotor(1.0)
-    v = 0.25
+    Ωᵢ = 2e-3
+    v = PostNewtonian.v(Ω=Ωᵢ, M=M₁+M₂)
     state = [M₁, M₂, vec(χ⃗₁)..., vec(χ⃗₂)..., components(R)..., v]
 
     Ω₊, Ω₋ = up_down_instability(state)
-    @test 0 ≤ Ω₊ ≤ Ω₋ ≤ 1  # General property
-    @test Ω₊ < 6e-4
-    @test Ω₋ == 1
+
+    # Test this general property, which should be true of *every* value returned for *any*
+    # inputs to this function:
+    @test 0 ≤ Ω₊ ≤ Ω₋ ≤ PostNewtonian.Ω(v=1, M=M₁+M₂)
+
+    # Now, for the specific set of parameters given above, we should have
+    @test Ω₊ ≈ 5.46e-4 rtol=1e-2
+    @test Ω₋ ≈ PostNewtonian.Ω(v=1, M=M₁+M₂)
 
     ## Check on the test in `inspiral` to make sure it works as expected
     # Ensure that even with `quiet=true`, we still get a warning for an unstable configuration
-    udi_warning = r"This system is likely to encounter the up-down instability in the\n"
+    udi_warning = r"\nThis system is likely to encounter the up-down instability in the"
     @test_logs (:warn, udi_warning) inspiral(M₁, M₂, χ⃗₁, χ⃗₂, Ω(v=v), quiet=true)
 
     # Ensure that incorrect mass ordering gets handled properly
