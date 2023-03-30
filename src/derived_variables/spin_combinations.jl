8d008d8f8ad8e454296f3d82cf08f9be0a50805b
user.email <michael.oliver.boyle@gmail.com>
2023-03-30 15:31:15 -0400

    Accept vectors as input to *Variables functions

--- src/derived_variables/spin_combinations.jl
+++ src/derived_variables/spin_combinations.jl
@@ -3,14 +3,14 @@
 
 Dimensionful spin vector of object 1.
 """
-S⃗₁(s::PNSystem) = χ⃗₁(s) * M₁(s)^2
+S⃗₁(s::VecOrPNSystem) = χ⃗₁(s) * M₁(s)^2
 
 """
     S⃗₂(pnsystem)
 
 Dimensionful spin vector of object 2.
 """
-S⃗₂(s::PNSystem) = χ⃗₂(s) * M₂(s)^2
+S⃗₂(s::VecOrPNSystem) = χ⃗₂(s) * M₂(s)^2
 
 """
     S⃗(pnsystem)
@@ -19,7 +19,7 @@ S⃗₂(s::PNSystem) = χ⃗₂(s) * M₂(s)^2
 Total (dimensionful) spin vector ``S⃗₁+S⃗₂``.
 """
 S⃗(M₁, M₂, χ⃗₁, χ⃗₂) = χ⃗₁ * M₁^2 + χ⃗₂ * M₂^2
-S⃗(s::PNSystem) = S⃗(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
+S⃗(s::VecOrPNSystem) = S⃗(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
 
 """
     Σ⃗(pnsystem)
@@ -28,7 +28,7 @@ S⃗(s::PNSystem) = S⃗(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
 Differential spin vector ``M(a⃗₂-a⃗₁)``.
 """
 Σ⃗(M₁, M₂, χ⃗₁, χ⃗₂) =  (M₁ + M₂) * (χ⃗₂ * M₂ - χ⃗₁ * M₁)
-Σ⃗(s::PNSystem) = Σ⃗(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
+Σ⃗(s::VecOrPNSystem) = Σ⃗(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
 
 """
     χ⃗(pnsystem)
@@ -37,7 +37,7 @@ Differential spin vector ``M(a⃗₂-a⃗₁)``.
 Normalized spin vector ``S⃗/M²``.
 """
 χ⃗(S⃗, M) = S⃗ / M^2
-χ⃗(s::PNSystem) = χ⃗(S⃗(s), M(s))
+χ⃗(s::VecOrPNSystem) = χ⃗(S⃗(s), M(s))
 
 """
     χ⃗ₛ(M₁, M₂, χ⃗₁, χ⃗₂)
@@ -45,7 +45,7 @@ Normalized spin vector ``S⃗/M²``.
 Symmetric spin vector ``(χ⃗₁+χ⃗₂)/2``.
 """
 χ⃗ₛ(M₁, M₂, χ⃗₁, χ⃗₂) = (χ⃗₁ + χ⃗₂) / 2
-χ⃗ₛ(s::PNSystem) = χ⃗ₛ(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
+χ⃗ₛ(s::VecOrPNSystem) = χ⃗ₛ(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
 
 """
     χ⃗ₐ(M₁, M₂, χ⃗₁, χ⃗₂)
@@ -53,30 +53,30 @@ Symmetric spin vector ``(χ⃗₁+χ⃗₂)/2``.
 Antisymmetric spin vector ``(χ⃗₁-χ⃗₂)/2``.
 """
 χ⃗ₐ(M₁, M₂, χ⃗₁, χ⃗₂) = (χ⃗₁ - χ⃗₂) / 2
-χ⃗ₐ(s::PNSystem) = χ⃗ₐ(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
+χ⃗ₐ(s::VecOrPNSystem) = χ⃗ₐ(M₁(s), M₂(s), χ⃗₁(s), χ⃗₂(s))
 
-χₚₑᵣₚ(s::PNSystem) = √(χ₁²(s) - (χ₁ₗ(s))^2 + χ₂²(s) - (χ₂ₗ(s))^2)
+χₚₑᵣₚ(s::VecOrPNSystem) = √(χ₁²(s) - (χ₁ₗ(s))^2 + χ₂²(s) - (χ₂ₗ(s))^2)
 
-χ₁²(s::PNSystem) = abs2vec(χ⃗₁(s))
-χ₂²(s::PNSystem) = abs2vec(χ⃗₂(s))
-χ₁(s::PNSystem) = absvec(χ⃗₁(s))
-χ₂(s::PNSystem) = absvec(χ⃗₂(s))
-χ₁₂(s::PNSystem) = χ⃗₁(s) ⋅ χ⃗₂(s)
-χ₁ₗ(s::PNSystem) = χ⃗₁(s) ⋅ ℓ̂(s)
-χ₂ₗ(s::PNSystem) = χ⃗₂(s) ⋅ ℓ̂(s)
-χₛₗ(s::PNSystem) = χ⃗ₛ(s) ⋅ ℓ̂(s)
-χₐₗ(s::PNSystem) = χ⃗ₐ(s) ⋅ ℓ̂(s)
+χ₁²(s::VecOrPNSystem) = abs2vec(χ⃗₁(s))
+χ₂²(s::VecOrPNSystem) = abs2vec(χ⃗₂(s))
+χ₁(s::VecOrPNSystem) = absvec(χ⃗₁(s))
+χ₂(s::VecOrPNSystem) = absvec(χ⃗₂(s))
+χ₁₂(s::VecOrPNSystem) = χ⃗₁(s) ⋅ χ⃗₂(s)
+χ₁ₗ(s::VecOrPNSystem) = χ⃗₁(s) ⋅ ℓ̂(s)
+χ₂ₗ(s::VecOrPNSystem) = χ⃗₂(s) ⋅ ℓ̂(s)
+χₛₗ(s::VecOrPNSystem) = χ⃗ₛ(s) ⋅ ℓ̂(s)
+χₐₗ(s::VecOrPNSystem) = χ⃗ₐ(s) ⋅ ℓ̂(s)
 
-Sₙ(s::PNSystem) = S⃗(s) ⋅ n̂(s)
-Σₙ(s::PNSystem) = Σ⃗(s) ⋅ n̂(s)
-Sλ(s::PNSystem) = S⃗(s) ⋅ λ̂(s)
-Σλ(s::PNSystem) = Σ⃗(s) ⋅ λ̂(s)
-Sₗ(s::PNSystem) = S⃗(s) ⋅ ℓ̂(s)
-Σₗ(s::PNSystem) = Σ⃗(s) ⋅ ℓ̂(s)
+Sₙ(s::VecOrPNSystem) = S⃗(s) ⋅ n̂(s)
+Σₙ(s::VecOrPNSystem) = Σ⃗(s) ⋅ n̂(s)
+Sλ(s::VecOrPNSystem) = S⃗(s) ⋅ λ̂(s)
+Σλ(s::VecOrPNSystem) = Σ⃗(s) ⋅ λ̂(s)
+Sₗ(s::VecOrPNSystem) = S⃗(s) ⋅ ℓ̂(s)
+Σₗ(s::VecOrPNSystem) = Σ⃗(s) ⋅ ℓ̂(s)
 
-S₁ₙ(s::PNSystem) = S⃗₁(s) ⋅ n̂(s)
-S₁λ(s::PNSystem) = S⃗₁(s) ⋅ λ̂(s)
-S₁ₗ(s::PNSystem) = S⃗₁(s) ⋅ ℓ̂(s)
-S₂ₙ(s::PNSystem) = S⃗₂(s) ⋅ n̂(s)
-S₂λ(s::PNSystem) = S⃗₂(s) ⋅ λ̂(s)
-S₂ₗ(s::PNSystem) = S⃗₂(s) ⋅ ℓ̂(s)
+S₁ₙ(s::VecOrPNSystem) = S⃗₁(s) ⋅ n̂(s)
+S₁λ(s::VecOrPNSystem) = S⃗₁(s) ⋅ λ̂(s)
+S₁ₗ(s::VecOrPNSystem) = S⃗₁(s) ⋅ ℓ̂(s)
+S₂ₙ(s::VecOrPNSystem) = S⃗₂(s) ⋅ n̂(s)
+S₂λ(s::VecOrPNSystem) = S⃗₂(s) ⋅ λ̂(s)
+S₂ₗ(s::VecOrPNSystem) = S⃗₂(s) ⋅ ℓ̂(s)
