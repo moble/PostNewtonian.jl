ee7c528b2ad713f908cd296716944ad037697ca6
user.email <michael.oliver.boyle@gmail.com>
2023-04-22 15:38:51 -0400

    Simplify angles; use @pn_expansion correctly

--- src/pn_expressions/tidal_heating.jl
+++ src/pn_expressions/tidal_heating.jl
@@ -1,4 +1,4 @@
-"""
+@doc raw"""
     tidal_heating(pnsystem)
 
 Compute the rate of energy and angular-momentum absorption into each black hole in a binary.
@@ -19,19 +19,111 @@ See also
   - [Poisson and Sasaki (1995)](https://arxiv.org/abs/gr-qc/9412027)
   - [Tagoshi et al. (1997)](https://arxiv.org/abs/gr-qc/9711072)
   - [Chatziioannou et al. (2012)](https://arxiv.org/abs/1211.1686)
+
+# Notes
+
+
+
+The hardest parts to compute here involve the relative angles between the spins and the
+black-hole separation vectors.  Alvi constructs a spherical coordinate system centered on
+each black hole where the ``z`` axis is given by the direction of the spin, and ``\theta``
+and ``\phi`` represent the direction to the other black hole.  While he makes a (somewhat
+ambiguous) choice about the origin of the ``\phi`` coordinate, only ``\dot{\phi}`` comes
+into the equations, so we don't really care about that origin.
+
+Note that Alvi uses ``\mathbf{n}`` to represent the "normal to the orbital plane", whereas
+we — and most of the rest of the post-Newtonian literature — use [``\hat{\ell}``](@ref
+PostNewtonian.ℓ̂) for this vector and [``\hat{n}``](@ref PostNewtonian.n̂) to represent the
+separation vector pointing *from* object 2 *to* object 1.  For convenience, we define
+```math
+\hat{n}_i = \begin{cases}
+-\hat{n} & \text{i=1}, \\
+\hphantom{-}\hat{n} & \text{i=2}.
+\end{cases}
+```
+
+Alvi's construction is also somewhat adiabatic, so we treat the spins and orbital plane as
+constant in the calculation of the instantaneous tidal heating — though they evolve slowly
+over time — and the angles ``\theta`` and ``\phi`` as evolving rapidly.  In our formulation,
+then, the goal is to find the angle ``\theta_i(t)`` between ``\vec{\chi}_i`` and
+``\hat{n}_i``, and the rotation rate ``\dot{\phi}_i(t)`` of ``\hat{n}_i`` about the
+``\vec{\chi}_i`` axis.
+
+Computing the angle between vectors is a somewhat infamously tricky problem.  There are
+various [claims](https://people.eecs.berkeley.edu/~wkahan/Triangle.pdf) floating around
+about the best ways to compute quantities involving areas and angles of triangles.  While
+these claims are [surely true for areas](https://inria.hal.science/hal-00790071), I am more
+skeptical of the relevance for angles.  I find it best to realize that you probably don't
+need the angle *per se*, but trigonometric functions of the angle — like ``\sin^2
+\theta_i``, which is what we actually need in this case.  In particular, I believe the best
+results come from computing
+```math
+\sin^2\theta_i = \frac{
+    \left|\hat{n}_i \times \vec{\chi}_i\right|^2
+}{
+    \left| \vec{\chi}_i\right|^2
+}.
+```
+If the denominator is zero, we set ``\sin^2\theta_i = 1`` from physical
+considerations.
+
+Now consider the quantity ``\hat{n}_i \times \vec{\chi}_i``.  We next aim to
+calculate the rotation rate ``\dot{\phi}_i`` of this vector about
+``\vec{\chi}_i``.  We begin by directly calculating
+```math
+\partial_t \left(\hat{n}_i \times \vec{\chi}_i\right)
+=
+\left(\partial_t \hat{n}_i\right) \times \vec{\chi}_i
+=
+\left(\Omega\, \hat{\ell} \times \hat{n}_i\right) \times \vec{\chi}_i
+=
+\mp \Omega\, \hat{\lambda} \times \vec{\chi}_i,
+```
+where the negative sign is chosen for ``i=1`` and positive for ``i=2``.  Now,
+from more fundamental considerations, we can understand the components of this
+change.  Since we assume that ``\vec{\chi}_i`` is constant at each instant for
+the purposes of calculation here, the only way for ``\hat{n}_i \times
+\vec{\chi}_i`` to change is either because ``\hat{n}_i`` rotates about
+``\vec{\chi}_i``, or because the angle ``\theta_i`` is changing.  We can express
+this as
+```math
+\partial_t \left(\hat{n}_i \times \vec{\chi}_i\right)
+=
+\left( \dot{\phi}\, \hat{\chi}_i \right) \times \left(\hat{n}_i \times \vec{\chi}_i\right)
++
+\dot{\theta_i}\, \cot \theta_i\, \left(\hat{n}_i \times \vec{\chi}_i\right).
+```
+Since these two components are orthogonal, we can obtain ``\dot{\phi}`` directly
+by taking the component of this quantity along ``\hat{\chi}_i \times
+\left(\hat{n}_i \times \vec{\chi}_i\right)``:
+```math
+\dot{\phi}_i
+=
+\frac{
+    \left( \Omega\, \hat{\lambda} \times \vec{\chi}_i \right)
+    \cdot
+    \left(
+        \vphantom{\hat{\lambda}} \hat{\chi}_i
+        \times
+        \left(\hat{n} \times \vec{\chi}_i\right)
+    \right)
+}{
+    \left| \hat{\chi}_i \times \left(\hat{n} \times \vec{\chi}_i\right) \right|^2
+}
+=
+\Omega\, \frac{\hat{\ell} \cdot \hat{\chi}_i}{\sin^2 \theta_i}.
+```
+Here again, we may run into numerical trouble if ``\left| \vec{\chi}_1 \right|
+\approx 0``, in which case we again use physical arguments to take
+``\dot{\phi}_i = \Omega``.  However, we can also run into trouble if ``\sin^2
+\theta_i \approx 0``, which corresponds to a polar orbit, in which case Alvi's
+approximations break down.  In this case, we choose ``\dot{\phi}_i = 0``.
+
+Note that the sign of ``\hat{n}_i`` has dropped out of the calculations of both
+``\sin^2\theta_i`` and ``\dot{\phi}_i``.
 """
 @pn_expression function tidal_heating(pnsystem)
     # References to pages and equation numbers are from Alvi (2001)
-
-    # NOTE: Because Alvi's result is not written in the usual PN-expansion form, and because
-    # it's just a single term, I opt to just deal with the PN order here with a simple `if`
-    # switch, rather than `@pn_expansion` as usual.  In this case, the relevant order comes
-    # from the fact that Ṁ₁+Ṁ₂ gets added to the flux, so the 0PN term is v^10.  Note that
-    # b ∝ v^-2, and Ṁ₁ and Ṁ₂ ∝ v^12 (via I₀₁ and I₀₂).  So this is a 1PN term.  If
-    # higher-order terms are added in the future, this should be changed.
-    if pn_order(pnsystem) < 1
-        return eltype(pnsystem).((0,0,0,0))
-    end
 
     # Page 2, line 4
     rₕ₁ = M₁ * (1 + √(1-min(χ₁²,1)))
@@ -44,59 +136,24 @@ See also
     # Page 2, line 9
     b = M / v^2
 
-    # Equivalent to Eq. (18)
-    # θ is the angle between the BH's spin and the vector towards the opposite BH;
-    # ϕ̇ is the rotation rate of the plane containing those two vectors.
-
-    # Implementation note: Despite the claims of [Kahan
-    # (2014)](https://people.eecs.berkeley.edu/~wkahan/Triangle.pdf) and [Boldo
-    # (2013)](https://hal.inria.fr/hal-00790071), I actually find terrible numerical
-    # accuracy if I compute sin²θ using Kahan's method.  Maybe Julia is optimizing
-    # something that violates the order of operations Kahan prescribed.  But I find very
-    # good results using the simpler formula θ = atan √( |a⃗×b⃗|² / (a⃗⋅b⃗)² ), which is
-    # simpler in any case, so that's what I use.  Or more precisely, I use
-    # sin²(atan(n/d)) = n²/(d²+n²) with that formula.
-
-    sin²θ₁ = let cross2 = abs2vec(n̂×χ⃗₁)
-        denominator = cross2 + (n̂⋅χ⃗₁)^2
-        ifelse(
-            iszero(denominator),
-            one(cross2),
-            cross2 / denominator
-        )
-    end
-    sin²θ₂ = let cross2 = abs2vec(n̂×χ⃗₂)
-        denominator = cross2 + (n̂⋅χ⃗₂)^2
-        ifelse(
-            iszero(denominator),
-            one(cross2),
-            cross2 / denominator
-        )
-    end
-    ϕ̇₁ = let denominator = χ₁ * sin²θ₁
-        ifelse(
-            iszero(denominator),
-            Ω,
-            Ω * ℓ̂ ⋅ χ⃗₁ / denominator
-        )
-    end
-    ϕ̇₂ = let denominator = χ₂ * sin²θ₂
-        ifelse(
-            iszero(denominator),
-            Ω,
-            Ω * ℓ̂ ⋅ χ⃗₂ / denominator
-        )
-    end
+    # Equivalent to Eqs. (18)/(19)
+    #   * θ is the angle between the BH's spin and the vector towards the opposite BH;
+    #   * ϕ̇ is the rotation rate of the plane containing those two vectors.
+    # See docstring for details.
+    sin²θ₁ = ifelse(iszero(χ₁²), one(χ₁²), abs2vec(n̂×χ⃗₁)/χ₁²)
+    sin²θ₂ = ifelse(iszero(χ₂²), one(χ₂²), abs2vec(n̂×χ⃗₂)/χ₂²)
+    ϕ̇₁╱Ω = ifelse(iszero(χ₁²), one(χ₁²), ifelse(iszero(sin²θ₁), zero(χ₁²), ℓ̂⋅χ⃗₁/(χ₁*sin²θ₁)))
+    ϕ̇₂╱Ω = ifelse(iszero(χ₂²), one(χ₂²), ifelse(iszero(sin²θ₂), zero(χ₂²), ℓ̂⋅χ⃗₂/(χ₂*sin²θ₂)))
 
     # Eq. (10)
     I₀₁ = (16rₕ₁ / 5b^6) * M₁^5 * M₂^2 * sin²θ₁ * (1 - 3//4 * χ₁² + 15//4 * χ₁² * sin²θ₁)
     I₀₂ = (16rₕ₂ / 5b^6) * M₂^5 * M₁^2 * sin²θ₂ * (1 - 3//4 * χ₂² + 15//4 * χ₂² * sin²θ₂)
 
     # Eq. (21)
-    Ṡ₁ = (ϕ̇₁ - Ωₕ₁) * I₀₁
-    Ṁ₁ = ϕ̇₁ * Ṡ₁
-    Ṡ₂ = (ϕ̇₂ - Ωₕ₂) * I₀₂
-    Ṁ₂ = ϕ̇₂ * Ṡ₂
+    Ṡ₁ = I₀₁ * @pn_expansion (v^3 * ϕ̇₁╱Ω - Ωₕ₁)
+    Ṁ₁ = I₀₁ * v^3 * ϕ̇₁╱Ω * @pn_expansion 5 (v^3 * ϕ̇₁╱Ω - Ωₕ₁)  # ϕ̇₁ * Ṡ₁
+    Ṡ₂ = I₀₂ * @pn_expansion (v^3 * ϕ̇₂╱Ω - Ωₕ₂)
+    Ṁ₂ = I₀₂ * v^3 * ϕ̇₂╱Ω * @pn_expansion 5 (v^3 * ϕ̇₂╱Ω - Ωₕ₂)  # ϕ̇₂ * Ṡ₂
 
     (Ṡ₁, Ṁ₁, Ṡ₂, Ṁ₂)
 end
