6158ddf69a6cd23cdc879ef5551fb765553626d3
Mike Boyle <michael.oliver.boyle@gmail.com>
2024-06-28 15:00:34 -0400

    Update list of allowed stale imports

--- test/aqua.jl
+++ test/aqua.jl
@@ -7,7 +7,10 @@ end
     #@test ExplicitImports.check_all_explicit_imports_are_public(PostNewtonian) === nothing
     @test ExplicitImports.check_no_stale_explicit_imports(
         PostNewtonian;
-        ignore=(:M, :R, :X₁, :X₂, :q, :δ, :μ, :ν, :χ⃗₁, :χ⃗₂, :ℳ,),
+        ignore=(
+            :M, :R, :X₁, :X₂, :q, :δ, :μ, :ν, :χ⃗₁, :χ⃗₂, :ℳ,
+            :ln, :ln2, :ln3, :ln5, :order_index, :γₑ, :ζ3
+        ),
     ) === nothing
     @test ExplicitImports.check_all_qualified_accesses_via_owners(PostNewtonian) === nothing
     #@test ExplicitImports.check_all_qualified_accesses_are_public(PostNewtonian) === nothing
