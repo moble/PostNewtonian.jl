018ae341395d50ebcd85d640f24dba36e5782cc9
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-05-20 22:57:49 -0400

    Precompile one more branch

--- apps/precompilation_statements.jl
+++ apps/precompilation_statements.jl
@@ -1,5 +1,8 @@
 using ZeroEccParamsFromPN: ZeroEccParamsFromPN
 
-args = ["--q=4.3", "--chiA=0.1,0.2,0.3", "--chiB=0.3,0.2,0.1", "--Omega0=0.015"]
+basic_args = ["--q=4.3", "--chiA=0.1,0.2,0.3", "--chiB=0.3,0.2,0.1"]
 
-ZeroEccParamsFromPN.julia_main(args)
+for arg ∈ ["--Omega0=0.015", "--D0=15"]
+    args = [basic_args; arg]
+    ZeroEccParamsFromPN.julia_main(args)
+end
