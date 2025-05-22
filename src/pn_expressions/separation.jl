fcc9348476070296c6466d4b5a857c1a13bbbaab
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-05-22 14:33:59 -0400

    Fix the separation-related computations

--- src/pn_expressions/separation.jl
+++ src/pn_expressions/separation.jl
@@ -1,25 +1,24 @@
 @doc raw"""
-    γₚₙ(pnsystem)
-    inverse_separation(pnsystem)
+    γₚₙ₀(pnsystem, [r₀′])
 
-Compute the post-Newtonian parameter
-```math
-\gamma_{\mathrm{PN}} \equiv \frac{G\, M}{r\, c^2},
-```
-where ``r`` is the magnitude of the orbital separation.  This quantity has PN order 1, and
-is given by Eq. (4.3) of [Bohé et al. (2013)](https://arxiv.org/abs/1212.5520) and Eq.
-(3.32) of [Bohé et al.  (2015)](https://arxiv.org/abs/1501.01529).
+This is a helper function for [`γₚₙ`](@ref); this computes the portion of `γₚₙ` that does
+not depend on the logarithm of the separation.  Since `γₚₙ` is defined in terms of the
+separation, that term makes the standard definition into an implicit function.
 
-Note that there is a 3PN gauge term of ``-22ν\ln(r/r₀')/3``.  While this value should cancel
-out of any physical quantity, it is included here for completeness.  Computing it requires a
-few Newton steps to get the value of ``γ`` because the ``\ln(r)`` term depends on
-``\gamma``.
+Specifically, `γₚₙ = γₚₙ₀ - (v/c)^8 * (22ln(r*c^2/(G*M)) / 3)ν`, where the argument of the
+logarithm is precisely `1/γₚₙ` — by definition.  Note that there is a term just like the
+second term in the expression that involves `r₀′`, which is a gauge parameter.  By default,
+that term is simply ignored in this function, but if the optional argument is provided, it
+is included.  This combination of the logarithms involving `r` and `r₀′` should drop out of
+the result for any physical quantity.
 
-The default value of ``r₀'`` is precisely whatever is required to make *its* logarithm
-vanish (not the logarithm of `r`).  If you pass the optional argument `lnr₀′c²╱GM`, *you*
-are responsible for ensuring that the appropriate values of `M`, `c`, and `G` are used.
 """
-@pn_expression function γₚₙ(pnsystem, lnr₀′c²╱GM=0)
+@pn_expression function γₚₙ₀(pnsystem, r₀′=0)
+    lnr₀′c²╱GM = if iszero(r₀′)
+        0
+    else
+        ln(r₀′ * c^2 / (G * M))
+    end
     γ₀ = (v / c)^2 * @pn_expansion(
         # Non-spinning terms; Eq. (4.3) of Bohé et al. (2013)
         1 +
@@ -51,33 +50,68 @@ are responsible for ensuring that the appropriate values of `M`, `c`, and `G` ar
                 σₗ^2 * (1 + (δ * κ₋ - κ₊ - 2)ν + (κ₊ / 6 + 1//3)ν^2)
             )
     )
+end
+
+
+@doc raw"""
+    γₚₙ(pnsystem, [r₀′])
+    inverse_separation(pnsystem, [r₀′])
+
+Compute the post-Newtonian parameter
+```math
+\gamma_{\mathrm{PN}} \equiv \frac{G\, M}{r\, c^2},
+```
+where ``r`` is the magnitude of the orbital separation.  This quantity has PN order 1, and
+is given by Eq. (4.3) of [Bohé et al. (2013)](https://arxiv.org/abs/1212.5520) and Eq.
+(3.32) of [Bohé et al.  (2015)](https://arxiv.org/abs/1501.01529).
+
+Note that there is a 3PN gauge term of ``-22ν\ln(r/r₀')/3``.  While this value should cancel
+out of any physical quantity, it is included here for completeness.  Computing it requires a
+few Newton steps to get the value of ``γ`` because the ``\ln(r)`` term depends on
+``\gamma``.
+
+Specifically, we use the helper function [`γₚₙ₀`](@ref) to write `γₚₙ = γₚₙ₀ + (v/c)^8 *
+(22ln(γₚₙ)/3)ν`; given the value of `γₚₙ₀`, the purpose of this function is to determine
+`γₚₙ`.
+
+The default value of `r₀'` is `G*M/c^2`, which makes its contribution vanish.
+"""
+@pn_expression function γₚₙ(pnsystem, r₀′=0)
+    γ₀ = γₚₙ₀(pnsystem, r₀′; pn_expansion_reducer)
 
     if pn_order(pnsystem) ≥ 3
-        if !isa(pn_expansion_reducer, Val{sum})
-            throw(ArgumentError(
-                "`PostNewtonian.γₚₙ` not implemented for `pn_expansion_reducer` types other"
-                *" than `Val{sum}`.  (That is, you can't get individual terms out of this.)"
-            ))
-        end
-
         # Account for the 3PN gauge term.  Note that the coefficient of the logarithm is
         # too small for the Lambert W function to give us a useful result, so we just
         # do a few Newton steps to get the value of γ = γ₀ + (v/c)^8 * (22ln(γ) / 3)ν
         a = (v / c)^8 * (22ν / 3)
-        γᵢ = γ₀
-        for i ∈ 1:10  # Limit the possible number of steps, just in case
-            δγ = - (γᵢ + a*ln(γᵢ) - γ₀) / (1 + a / γᵢ)
-            γᵢ += δγ
-            # if abs(δγ) < 10eps(γᵢ)
-            #     break
-            # end
+        Δγ = let γ₀ = sum(γ₀)
+            Δγᵢ = zero(a*ln(γ₀))
+            for i ∈ 1:20  # Limit the possible number of steps, just in case things break
+                γᵢ = γ₀ + Δγᵢ
+                δγ = - (Δγᵢ + a*ln(γᵢ)) / (1 + a / (γᵢ))
+                Δγᵢ += δγ
+                if abs(δγ) < 10eps(γᵢ)
+                    break
+                end
+            end
+            Δγᵢ
         end
-        return γᵢ
+        if isa(pn_expansion_reducer, Val{sum})
+            return γ₀ + Δγ
+        else
+            # γ₀ will be a `PNExpansion`, and we need to add the `Δγ` to term 7
+            coeffs = Tuple(
+                i==7 ? c + Δγ : c
+                for (i,c) ∈ enumerate(γ₀.coeffs)
+            )
+            return typeof(γ₀)(coeffs)
+        end
     else
         return γ₀
     end
 end
 const inverse_separation = γₚₙ
+
 
 @doc raw"""
     γₚₙ′(pnsystem)
@@ -85,8 +119,51 @@ const inverse_separation = γₚₙ
 
 Compute the derivative of [`γₚₙ`](@ref) with respect to `v`.
 
+Note that we ignore the `r₀′` term in this function; that constant is obviously independent
+of `v`, though it is multiplied by `M`, which is not independent of `v`.  This dependence,
+however, should be at a much higher PN order than is currently available in any case, so we
+ignore it for simplicity.
+
+This computation uses [`γₚₙ₀`](@ref) along with the following derivation.
+```math
+\begin{align*}
+γₚₙ &= γₚₙ₀ + (v/c)^8 (22 \ln(γₚₙ)/3)ν \\
+γₚₙ' &= γₚₙ₀' + 8(v/c)^7 (22 \ln(γₚₙ)/3)ν + (v/c)^8 (22 γₚₙ'/3γₚₙ)ν \\
+γₚₙ' &= \frac{γₚₙ₀' + 8(v/c)^7 (22 \ln(γₚₙ)/3)ν} {1 - (v/c)^8 (22/3γₚₙ)ν}
+\end{align*}
+```
+
 """
-@generated function γₚₙ′(
+@pn_expression function γₚₙ′(pnsystem)
+    if !isa(pn_expansion_reducer, Val{sum})
+        throw(ArgumentError(
+            "`PostNewtonian.γₚₙ′` not implemented for `pn_expansion_reducer` types other"
+            *" than `Val{sum}`.  (That is, you can't get individual terms out of this.)"
+        ))
+    end
+
+    γ₀′ = γₚₙ₀′(pnsystem)
+
+    if pn_order(pnsystem) ≥ 3
+        let γₚₙ = γₚₙ(pnsystem)
+            (γ₀′ + 8(v / c)^7 * (22ln(γₚₙ) / 3)ν) / (1 - (v / c)^8 * (22 / 3γₚₙ) * ν)
+        end
+    else
+        γ₀′
+    end
+end
+const inverse_separation_deriv = γₚₙ′
+
+
+@doc raw"""
+    γₚₙ₀′(pnsystem)
+
+Helper function to compute [`γₚₙ′`](@ref).  This just computes the derivative of
+[`γₚₙ₀`](@ref) with respect to `v`; `γₚₙ′` takes care of the extra complications arising
+from the Newton iterations in [`γₚₙ′`](@ref).
+
+"""
+@generated function γₚₙ₀′(
     pnsystem::PNSystem{ST,PNOrder}; pn_expansion_reducer::Val{PNExpansionReducer}=Val(sum)
 ) where {ST,PNOrder,PNExpansionReducer}
     # Create a `PNSystem` with `FastDifferentiation` (henceforth FD) variables, using the
@@ -100,62 +177,62 @@ Compute the derivative of [`γₚₙ`](@ref) with respect to `v`.
     # Now we evaluate γₚₙ using the FD variables.  This will expand all derived variables in
     # terms of the fundamental variables, but FD will take care of evaluating those
     # efficiently via common subexpression elimination (CSE).
-    γₚₙformula = γₚₙ(fdpnsystem; pn_expansion_reducer=Val(PNExpansionReducer))
+    γₚₙ₀formula = γₚₙ₀(fdpnsystem; pn_expansion_reducer=Val(PNExpansionReducer))
 
-    # Now we take the derivative of γₚₙ with respect to v.
-    γₚₙ′ = SVector(FastDifferentiation.derivative(γₚₙformula, v(fdpnsystem)))
+    # Now we take the derivative of γₚₙ₀ with respect to v.
+    γₚₙ₀′ = SVector(FastDifferentiation.derivative(γₚₙ₀formula, v(fdpnsystem)))
 
     # Turn that into an Expr (FD insists on making it a function)
     in_place = true
     init_with_zeros = false
-    γₚₙ′expr = FastDifferentiation.make_Expr(γₚₙ′, vars, in_place, init_with_zeros)
+    γₚₙ₀′expr = FastDifferentiation.make_Expr(γₚₙ₀′, vars, in_place, init_with_zeros)
 
     # Now, we use `MacroTools` to get the body of the function.
-    γₚₙ′body = MacroTools.unblock(MacroTools.splitdef(γₚₙ′expr)[:body])
+    γₚₙ₀′body = MacroTools.unblock(MacroTools.splitdef(γₚₙ₀′expr)[:body])
 
     # # At this point, the function is just a long series of statements inside an `@inbounds`
     # # block, which we will want later, but first we need to extract them.
-    MacroTools.@capture(γₚₙ′body, @inbounds begin
-        γₚₙ′statements__
+    MacroTools.@capture(γₚₙ₀′body, @inbounds begin
+        γₚₙ₀′statements__
     end) || throw(
         ArgumentError(
-            "\nNo @inbounds block found in γₚₙ′ expression." *
+            "\nNo @inbounds block found in γₚₙ₀′ expression." *
             "\nSomething may have changed in FastDifferentiation." *
             "\nOpen an issue citing this Julia call:" *
             "\n```julia" *
             "\nusing PostNewtonian" *
-            "\nγₚₙ′($pnsystem)" *
+            "\nγₚₙ₀′($pnsystem)" *
             "\n```",
         ),
     )
 
-    # The γₚₙ′statements are mostly what we want, except that the last line is a return
+    # The γₚₙ₀′statements are mostly what we want, except that the last line is a return
     # statement.  We want that result, but we don't to return it yet; we want to wrap that
     # result, so we just get that returned quantity here.
-    MacroTools.@capture(γₚₙ′statements[end], return γₚₙ′return_) || throw(
+    MacroTools.@capture(γₚₙ₀′statements[end], return γₚₙ₀′return_) || throw(
         ArgumentError(
             "\nNo return statement found in γₚₙ′ expression." *
             "\nSomething may have changed in FastDifferentiation." *
             "\nOpen an issue citing this Julia call:" *
             "\n```julia" *
             "\nusing PostNewtonian" *
-            "\nγₚₙ′($pnsystem)" *
+            "\nγₚₙ₀′($pnsystem)" *
             "\n```",
         ),
     )
-    γₚₙ′statements[end] = γₚₙ′return
+    γₚₙ₀′statements[end] = γₚₙ₀′return
 
     if PNExpansionReducer === identity
         # When `pn_expansion_reducer=Val(identity)` is passed, we return a PNExpansion
         NMax = Int(2PNOrder + 1)
         return quote
             input_variables = SVector(pnsystem)
-            result = MVector{$(length(γₚₙ′)),$(eltype(ST))}(undef)
+            result = MVector{$(length(γₚₙ₀′)),$(eltype(ST))}(undef)
             result .= 0
             @fastmath @inbounds begin
-                $(γₚₙ′statements...)
+                $(γₚₙ₀′statements...)
             end
-            return PNExpansion{$(length(γₚₙ′)),$(eltype(ST)),$NMax}(Tuple(result))
+            return PNExpansion{$(length(γₚₙ₀′)),$(eltype(ST)),$NMax}(Tuple(result))
         end
     else
         # Otherwise, FD produces a 1-tuple, so we just extract the value from that.
@@ -164,57 +241,65 @@ Compute the derivative of [`γₚₙ`](@ref) with respect to `v`.
             result = MVector{1,$(eltype(ST))}(undef)
             result .= 0
             @fastmath @inbounds begin
-                $(γₚₙ′statements...)
+                $(γₚₙ₀′statements...)
             end
             return result[1]
         end
     end
 end
-const inverse_separation_deriv = γₚₙ′
+
 
 """
-    r(pnsystem)
-    separation(pnsystem)
+    r(pnsystem, [r₀′])
+    separation(pnsystem, [r₀′])
 
 Compute the separation between the two black holes.  This is essentially the multiplicative
 inverse of [`γₚₙ`](@ref), with some factors of `G` and `M` thrown in.
-
-Note that there should be a factor of `1/c^2` in this expression; we reserve it to use
-explicitly in PN expansions.  That is, for every factor of `1/r`, we explicitly include a
-factor of `1/c^2` in the expansion.
 """
-@pn_expression function r(pnsystem)
-    let γₚₙ = γₚₙ(pnsystem)
+@pn_expression function r(pnsystem, r₀′=0)
+    let γₚₙ = γₚₙ(pnsystem, r₀′)
         return G * M / (γₚₙ * c^2)
     end
 end
 const separation = r
 
+
 """
-    r′(pnsystem)
-    separation_deriv(pnsystem)
+    r′(pnsystem, [r₀′])
+    separation_deriv(pnsystem, [r₀′])
 
 Compute the derivative of the separation between the two black holes with respect to `v`.
+
+Note that we ignore a derivative of `M` that appears in the `r₀′` term, as explained in
+[`γₚₙ′`](@ref).
 """
-@pn_expression function r′(pnsystem)
-    let γₚₙ = γₚₙ(pnsystem)
+@pn_expression function r′(pnsystem, r₀′=0)
+    let γₚₙ = γₚₙ(pnsystem, r₀′), γₚₙ′ = γₚₙ′(pnsystem)
         -γₚₙ′ * G * M / (γₚₙ^2 * c^2)
     end
 end
 const separation_deriv = r′
 
-@pn_expression function ṙ(pnsystem)
-    let γₚₙ = γₚₙ(pnsystem), γₚₙ′ = γₚₙ′(pnsystem), 𝓕 = 𝓕(pnsystem), 𝓔′ = 𝓔′(pnsystem)
+
+"""
+    ṙ(pnsystem, [r₀′])
+    separation_dot(pnsystem, [r₀′])
+
+Compute the derivative of the separation between the two black holes with respect to time.
+"""
+@pn_expression function ṙ(pnsystem, r₀′=0)
+    let γₚₙ = γₚₙ(pnsystem, r₀′), γₚₙ′ = γₚₙ′(pnsystem), 𝓕 = 𝓕(pnsystem), 𝓔′ = 𝓔′(pnsystem)
         𝓕 * γₚₙ′ * G * M / (γₚₙ^2 * c^2 * 𝓔′)
     end
 end
 const separation_dot = ṙ
 
-"""
-    γₚₙ⁻¹(γ, pnsystem)
-    inverse_separation_inverse(γ, pnsystem)
 
-Return `v` such that `γₚₙ(pnsystem) = γ` when `pnsystem` is evaluated at `v`.
+"""
+    γₚₙ⁻¹(γ, pnsystem, [r₀′])
+    inverse_separation_inverse(γ, pnsystem, [r₀′])
+
+Return `v` such that `γₚₙ(pnsystem, r₀′) = γ` when `pnsystem` is evaluated at `v`.
 
 Note that the value of `v` in the input `pnsystem` is ignored; you may use any value.  It
 may also be convenient to know that you can set the value of `v` in `pnsystem` to the
@@ -224,53 +309,50 @@ pnsystem.state[PostNewtonian.vindex] = γₚₙ⁻¹(γ, pnsystem)
 ```
 See also [`r⁻¹`](@ref).
 """
-function γₚₙ⁻¹(γ, pnsystem)
+function γₚₙ⁻¹(γ, pnsystem, r₀′=0)
     if 2γ ≥ 1
-        @info "Error with" pnsystem
+        @error "Error in γₚₙ⁻¹" γ pnsystem
         throw(ArgumentError("γ=$γ ≥ 1/2 describes a binary that has already merged"))
     elseif γ ≤ 0
-        @info "Error with" pnsystem
+        @error "Error in γₚₙ⁻¹" γ pnsystem
         throw(ArgumentError("γ=$γ ≤ 0 is unphysical"))
     end
 
-    # We evaluate at v=1 just to get all the terms out separately, without actually multiplying
-    # by the powers of v.
-    pn = deepcopy(pnsystem)
-    pn.state[vindex] = one(eltype(pn))
+    pnsystemᵥ = deepcopy(pnsystem)
 
-    # Now we can get the actual terms.  Note that there is a pre-factor of (v/c)^2.
-    γₚₙ_expansion = γₚₙ(pn; pn_expansion_reducer=Val(identity))
+    function newton_step(v, γ, pnsystemᵥ)
+        # We denote by pnystemᵥ the `pnsystem` with the value of `v` set to this function's
+        # argument, the trial value `v`.  We take a Newton step to find the value of `v`
+        # such that γₚₙ(pnsystemᵥ) = γ; that is, we're finding the root of
+        #   f(v) = γₚₙ(pnsystemᵥ) - γ
+        # with
+        #   f′(v) = γₚₙ′(pnsystemᵥ)
+        pnsystemᵥ.state[vindex] = v
+        γᵥ = γₚₙ(pnsystemᵥ, r₀′)
+        γᵥ′ = γₚₙ′(pnsystemᵥ)
+        return -( (γᵥ - γ) / γᵥ′ )
+    end
 
-    # Include the pre-factor of (v/c)^2, then compute coefficients of the first and second
-    # derivatives with respect to v.
-    coeffs = (0.0, 0.0, γₚₙ_expansion.coeffs...)
-    coeffs′ = Tuple(i * c for (i, c) ∈ enumerate(coeffs[2:end]))
-    coeffs′′ = Tuple(i * c for (i, c) ∈ enumerate(coeffs′[2:end]))
-
-    # Defining the cost function as Ξ(v) = (evalpoly(v, coeffs) - γ)^2, the Newton step is
-    # -Ξ′(v) / Ξ′′(v), which is easy to compute from the coefficients:
-    function newton_step(v)
-        return -(
-            (evalpoly(v, coeffs) - γ) * evalpoly(v, coeffs′) /
-            ((evalpoly(v, coeffs) - γ) * evalpoly(v, coeffs′′) + (evalpoly(v, coeffs′))^2)
-        )
+    # We can get an initial guess by solving the leading-order equation
+    # γₚₙ = v² + v²^2 * (1 - ν / 3) for v.  The quadratic formula gives us
+    #   v² = (-1 ± √(1 + 4(1 - ν / 3)γ)) / (2(1 - ν / 3))
+    # We obviously want the result to be positive, so we take the positive root.  We don't
+    # need to worry about signs in these square-roots because (1-ν/3) will always be
+    # between 1 and 11/12, and γ will always be strictly positive.
+    vᵢ = let ν = ν(pnsystem)
+        √((-1 + √(1 + 4(1 - ν / 3)γ)) / (2(1 - ν / 3)))
     end
 
     # Now we just do a few Newton steps to get the value of v.
-    vᵢ = let ν = ν(pnsystem)
-        try
-            √((3 - √(-12ν * γ + 36γ + 9)) / (2ν - 6))
-        catch
-            return zero(γ)
-            # @info γ pnsystem
-            # rethrow
-        end
-    end
-    for i ∈ 1:10  # Limit the possible number of steps, just in case
-        δvᵢ = newton_step(vᵢ)
+    maxNsteps = 50  # Limit the possible number of steps, just in case...
+    for i ∈ 1:maxNsteps
+        δvᵢ = newton_step(vᵢ, γ, pnsystemᵥ)
         vᵢ += δvᵢ
         if abs(δvᵢ) < 10eps(vᵢ)
             break
+        end
+        if i==maxNsteps
+            @error "Failure in γₚₙ⁻¹: Failed to converge after $i iterations" γ pnsystem vᵢ
         end
     end
 
@@ -278,9 +360,10 @@ function γₚₙ⁻¹(γ, pnsystem)
 end
 const inverse_separation_inverse = γₚₙ⁻¹
 
+
 """
-    r⁻¹(r, pnsystem)
-    separation_inverse(r, pnsystem)
+    r⁻¹(r, pnsystem, [r₀′])
+    separation_inverse(r, pnsystem, [r₀′])
 
 Return `v` such that `r = r(v)` when `pnsystem` is evaluated at `v`.
 
@@ -292,13 +375,14 @@ pnsystem.state[PostNewtonian.vindex] = r⁻¹(r, pnsystem)
 ```
 See also [`γₚₙ⁻¹`](@ref).
 """
-function r⁻¹(r, pnsystem)
+function r⁻¹(r, pnsystem, r₀′=0)
     let c = 1, G = 1, M = M(pnsystem)
         γ = G * M / (r * c^2)
-        v = γₚₙ⁻¹(γ, pnsystem)
+        v = γₚₙ⁻¹(γ, pnsystem, r₀′)
     end
 end
 const separation_inverse = r⁻¹
+
 
 """
 This module contains a few expressions from [Kidder
