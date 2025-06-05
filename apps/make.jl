6ac99bc6ca532cfe03444b8177561ff712041b16
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-06-05 14:15:15 -0400

    Ensure that PostNewtonian is dev-ed into the app project

--- apps/make.jl
+++ apps/make.jl
@@ -1,7 +1,17 @@
 using Pkg
+
+# Ensure that the project inside the `ZeroEccParamsFromPN` directory is instantiated
+# and working with the latest code.
+Pkg.activate(joinpath(@__DIR__, "ZeroEccParamsFromPN"))
+Pkg.develop(; path=dirname(@__DIR__))
+Pkg.instantiate()
+
+# Now switch to *this* project and ensure that it is instantiated.
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
 
+# Create the app.  This may take ~5 minutes, and should create a bundle in this directory
+# called `ZeroEccParamsFromPNApp`.
 using PackageCompiler
 create_app(
     "ZeroEccParamsFromPN",
