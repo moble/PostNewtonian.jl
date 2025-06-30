78d5d6a90e4d40760da30e838332993f72a48818
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-06-30 01:01:47 -0400

    Add example literature entry

---
+++ src/literature/Einstein1918/Einstein1918.jl
@@ -0,0 +1,10 @@
+@pn_reference module Einstein1918
+
+import PostNewtonian: G, c, M₁, M₂, χ⃗₁, χ⃗₂, R, v, Φ
+import CommonVariables: χₛ
+
+# import common variables
+# define variables not in common variables
+# write individual expressions as separate functions with `@pn_expression`
+
+end
